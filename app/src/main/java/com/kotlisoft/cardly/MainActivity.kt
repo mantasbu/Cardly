0078c12613af2f434d825c349154197641b660b9
Mantas <Mantasb77@gmail.com>
2024-04-28 07:39:57 +0100

    Removed Surface component

--- app/src/main/java/com/kotlisoft/cardly/MainActivity.kt
+++ app/src/main/java/com/kotlisoft/cardly/MainActivity.kt
@@ -3,24 +3,13 @@ package com.kotlisoft.cardly
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import com.kotlisoft.cardly.ui.theme.CardlyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CardlyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
-            }
+            CardlyTheme {}
         }
     }
 }
