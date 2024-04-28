4b7f48f12a2920e793b8b815fb74b445634ae87e
Mantas <Mantasb77@gmail.com>
2024-04-28 07:45:32 +0100

    Added navigation component with initial destination

--- app/src/main/java/com/kotlisoft/cardly/MainActivity.kt
+++ app/src/main/java/com/kotlisoft/cardly/MainActivity.kt
@@ -3,13 +3,26 @@ package com.kotlisoft.cardly
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.kotlisoft.cardly.ui.theme.CardlyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CardlyTheme {}
+            val navController = rememberNavController()
+            CardlyTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = "topics",
+                ) {
+                    composable(route = "topics") {
+
+                    }
+                }
+            }
         }
     }
 }
