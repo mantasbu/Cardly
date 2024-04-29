3f865475425717c66169e2a55da4af78f0df59f4
Mantas <Mantasb77@gmail.com>
2024-04-29 07:42:00 +0100

    Updated dao methods

--- app/src/main/java/com/kotlisoft/cardly/data/local/CardDao.kt
+++ app/src/main/java/com/kotlisoft/cardly/data/local/CardDao.kt
@@ -4,12 +4,20 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CardDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCard(cardEntity: CardEntity)
 
-    @Delete
-    suspend fun deleteCard(cardEntity: CardEntity)
+    @Query("SELECT * FROM cards WHERE id = :id")
+    suspend fun getCardById(id: Int): CardEntity
+
+    @Query("SELECT * FROM cards WHERE deckName = :deck")
+    fun getCardsByDeck(deck: String): Flow<List<CardEntity>>
+
+    @Query("DELETE FROM cards WHERE id = :id")
+    suspend fun deleteCardById(id: Int)
 }
