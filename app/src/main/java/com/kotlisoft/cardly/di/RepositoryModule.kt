9c36761306aa71606a25204bf1979c21e5d9fb69
Mantas <Mantasb77@gmail.com>
2024-05-09 07:29:45 +0100

    Replaced Binds with Provides

--- app/src/main/java/com/kotlisoft/cardly/di/RepositoryModule.kt
+++ app/src/main/java/com/kotlisoft/cardly/di/RepositoryModule.kt
@@ -1,26 +1,46 @@
 package com.kotlisoft.cardly.di
 
+import com.kotlisoft.cardly.data.local.CardsDatabase
 import com.kotlisoft.cardly.data.repository.CardRepositoryImpl
+import com.kotlisoft.cardly.data.repository.DeckRepositoryImpl
 import com.kotlisoft.cardly.domain.repository.CardRepository
 import com.kotlisoft.cardly.domain.repository.DeckRepository
-import dagger.Binds
+import com.kotlisoft.cardly.domain.use_case.AddDeck
+import com.kotlisoft.cardly.domain.use_case.DeckUseCases
+import com.kotlisoft.cardly.domain.use_case.DeleteDeckByName
+import com.kotlisoft.cardly.domain.use_case.GetDecks
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
-    @Binds
+object RepositoryModule {
+    @Provides
     @Singleton
-    abstract fun bindCardRepository(
-        cardRepositoryImpl: CardRepositoryImpl
-    ): CardRepository
+    fun provideCardRepository(
+        cardsDatabase: CardsDatabase,
+    ): CardRepository {
+        return CardRepositoryImpl(cardsDatabase.cardDao)
+    }
 
-    @Binds
+    @Provides
     @Singleton
-    abstract fun bindDeckRepository(
-        deckRepository: DeckRepository
-    ): DeckRepository
+    fun provideDeckRepository(
+        cardsDatabase: CardsDatabase,
+    ): DeckRepository {
+        return DeckRepositoryImpl(cardsDatabase.deckDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDeckUseCases(deckRepository: DeckRepository): DeckUseCases {
+        return DeckUseCases(
+            addDeck = AddDeck(deckRepository),
+            deleteDeckByName = DeleteDeckByName(deckRepository),
+            getDecks = GetDecks(deckRepository),
+        )
+    }
 }
