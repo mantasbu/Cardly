14b5afe6814813c155b9ce96172709ea5e60a4c5
Mantas <Mantasb77@gmail.com>
2024-06-08 07:07:34 +0100

    Created CardItem

---
+++ app/src/main/java/com/kotlisoft/cardly/presentation/ui/cards/CardItem.kt
@@ -0,0 +1,56 @@
+package com.kotlisoft.cardly.presentation.ui.cards
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kotlisoft.cardly.domain.model.Card
+
+@Composable
+fun CardItem(
+    card: Card,
+    onEditCard: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var isAnswerVisible by remember {
+        mutableStateOf(false)
+    }
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable {
+                isAnswerVisible = !isAnswerVisible
+            },
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(16.dp)
+                .height(32.dp),
+        ) {
+            Text(text = if (isAnswerVisible) card.answer else card.question)
+            Icon(
+                imageVector = Icons.Default.Edit,
+                contentDescription = null,
+                modifier = Modifier.clickable {
+                    onEditCard()
+                }
+            )
+        }
+    }
+}
