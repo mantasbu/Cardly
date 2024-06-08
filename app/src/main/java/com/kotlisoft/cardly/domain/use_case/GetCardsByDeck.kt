a7af4adaad49c7644cff39e648751cc7247f5b9c
Mantas <Mantasb77@gmail.com>
2024-06-08 07:02:13 +0100

    Updated GetCardsByDeck use case

--- app/src/main/java/com/kotlisoft/cardly/domain/use_case/GetCardsByDeck.kt
+++ app/src/main/java/com/kotlisoft/cardly/domain/use_case/GetCardsByDeck.kt
@@ -1,12 +1,14 @@
 package com.kotlisoft.cardly.domain.use_case
 
+import com.kotlisoft.cardly.domain.model.Card
 import com.kotlisoft.cardly.domain.repository.CardRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetCardsByDeck @Inject constructor(
     private val cardRepository: CardRepository,
 ) {
-    operator fun invoke(deckName: String) {
-        cardRepository.getCardsByDeck(deckName)
+    operator fun invoke(deckName: String): Flow<List<Card>> {
+        return cardRepository.getCardsByDeck(deckName)
     }
 }
