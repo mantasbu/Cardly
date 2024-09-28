56a1c6d5540ce1ee6dc32afac3f32b3f3e8b2126
Mantas <Mantasb77@gmail.com>
2024-09-28 20:29:26 +0100

    Added missing dependencies

--- app/src/main/java/com/kotlisoft/cardly/di/RepositoryModule.kt
+++ app/src/main/java/com/kotlisoft/cardly/di/RepositoryModule.kt
@@ -1,27 +1,41 @@
 package com.kotlisoft.cardly.di
 
+import android.content.Context
+import com.kotlisoft.cardly.data.local.AssetDataSource
 import com.kotlisoft.cardly.data.local.CardsDatabase
 import com.kotlisoft.cardly.data.repository.CardRepositoryImpl
 import com.kotlisoft.cardly.data.repository.DeckRepositoryImpl
+import com.kotlisoft.cardly.data.repository.SampleDataRepositoryImpl
+import com.kotlisoft.cardly.domain.preferences.Preferences
 import com.kotlisoft.cardly.domain.repository.CardRepository
 import com.kotlisoft.cardly.domain.repository.DeckRepository
+import com.kotlisoft.cardly.domain.repository.SampleDataRepository
 import com.kotlisoft.cardly.domain.use_case.AddCard
 import com.kotlisoft.cardly.domain.use_case.AddDeck
 import com.kotlisoft.cardly.domain.use_case.CardUseCases
+import com.kotlisoft.cardly.domain.use_case.DataImportUseCase
 import com.kotlisoft.cardly.domain.use_case.DeckUseCases
+import com.kotlisoft.cardly.domain.use_case.DeleteAllCards
 import com.kotlisoft.cardly.domain.use_case.DeleteAllCardsByDeckName
+import com.kotlisoft.cardly.domain.use_case.DeleteAllDecks
 import com.kotlisoft.cardly.domain.use_case.DeleteCard
 import com.kotlisoft.cardly.domain.use_case.DeleteDeckByName
 import com.kotlisoft.cardly.domain.use_case.GetCard
 import com.kotlisoft.cardly.domain.use_case.GetCardsByDeck
 import com.kotlisoft.cardly.domain.use_case.GetDeckWithCards
 import com.kotlisoft.cardly.domain.use_case.GetDecks
+import com.kotlisoft.cardly.domain.use_case.GetSettings
+import com.kotlisoft.cardly.domain.use_case.PreferencesUseCases
+import com.kotlisoft.cardly.domain.use_case.UpdateAnswerLocale
 import com.kotlisoft.cardly.domain.use_case.UpdateCard
 import com.kotlisoft.cardly.domain.use_case.UpdateCardLevel
 import com.kotlisoft.cardly.domain.use_case.UpdateDeckName
+import com.kotlisoft.cardly.domain.use_case.UpdateQuestionLocale
+import com.kotlisoft.cardly.domain.use_case.UpdateSampleDataImport
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -46,6 +60,20 @@ object RepositoryModule {
 
     @Provides
     @Singleton
+    fun provideSampleDataRepository(
+        assetDataSource: AssetDataSource,
+    ): SampleDataRepository {
+        return SampleDataRepositoryImpl(assetDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAssetDataSource(@ApplicationContext context: Context): AssetDataSource {
+        return AssetDataSource(context)
+    }
+
+    @Provides
+    @Singleton
     fun provideDeckUseCases(deckRepository: DeckRepository): DeckUseCases {
         return DeckUseCases(
             addDeck = AddDeck(deckRepository),
@@ -53,6 +81,7 @@ object RepositoryModule {
             deleteAllCardsByDeckName = DeleteAllCardsByDeckName(deckRepository),
             updateDeckName = UpdateDeckName(deckRepository),
             getDecks = GetDecks(deckRepository),
+            deleteAllDecks = DeleteAllDecks(deckRepository)
         )
     }
 
@@ -67,6 +96,24 @@ object RepositoryModule {
             getDeckWithCards = GetDeckWithCards(cardRepository),
             updateCard = UpdateCard(cardRepository),
             updateCardLevel = UpdateCardLevel(cardRepository),
+            deleteAllCards = DeleteAllCards(cardRepository)
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataImportUseCase(sampleDataRepository: SampleDataRepository): DataImportUseCase {
+        return DataImportUseCase(repository = sampleDataRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferencesUseCases(preferences: Preferences): PreferencesUseCases {
+        return PreferencesUseCases(
+            updateQuestionLocale = UpdateQuestionLocale(preferences),
+            updateAnswerLocale = UpdateAnswerLocale(preferences),
+            updateSampleDataImport = UpdateSampleDataImport(preferences),
+            getSettings = GetSettings(preferences),
         )
     }
 }
