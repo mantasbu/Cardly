e3c0c8a40dbe84c547492b0354f927d66de493fc
Mantas <Mantasb77@gmail.com>
2024-09-28 21:17:14 +0100

    Added TextToSpeech for English and Spanish

--- app/src/main/java/com/kotlisoft/cardly/presentation/MainActivity.kt
+++ app/src/main/java/com/kotlisoft/cardly/presentation/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.kotlisoft.cardly.presentation
 
+import android.content.Intent
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -13,13 +20,31 @@ import com.kotlisoft.cardly.presentation.ui.decks.HomeScreen
 import com.kotlisoft.cardly.presentation.ui.navigation.NavArgs
 import com.kotlisoft.cardly.presentation.ui.navigation.Routes
 import com.kotlisoft.cardly.presentation.ui.quiz.QuizScreen
+import com.kotlisoft.cardly.presentation.ui.settings.SettingLocale
+import com.kotlisoft.cardly.presentation.ui.settings.SettingsScreen
 import com.kotlisoft.cardly.presentation.ui.theme.CardlyTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+
+    private lateinit var textToSpeech: TextToSpeech
+    private lateinit var textToSpeechInSpanish: TextToSpeech
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        textToSpeech = TextToSpeech(this) { status ->
+            onInit(status)
+        }
+
+        textToSpeechInSpanish = TextToSpeech(this) { status ->
+            onInitSpanish(status)
+        }
+
         setContent {
             val navController = rememberNavController()
             CardlyTheme {
@@ -32,6 +57,9 @@ class MainActivity : ComponentActivity() {
                             onNavigateToDeckCards = {
                                 navController.navigate(route = "${Routes.CARDS.name}/$it")
                             },
+                            onNavigateToSettings = {
+                                navController.navigate(route = "${Routes.SETTINGS.name}/$it")
+                            }
                         )
                     }
                     composable(
@@ -48,9 +76,7 @@ class MainActivity : ComponentActivity() {
                             onNavigateToQuizScreen = {
                                 navController.navigate(route = "${Routes.QUIZ.name}/$it")
                             },
-                            onNavigateBack = {
-                                navController.popBackStack()
-                            },
+                            onNavigateBack = navController::popBackStack,
                         )
                     }
                     composable(
@@ -62,14 +88,76 @@ class MainActivity : ComponentActivity() {
                         ),
                     ) {
                         val deckName = it.arguments?.getString(NavArgs.DECK_NAME.name) ?: ""
+                        val settingsState by remember(viewModel.state.value) {
+                            mutableStateOf(viewModel.state.value)
+                        }
+                        viewModel.loadSettings()
                         QuizScreen(
                             deckName = deckName,
-                            onNavigateBack = {
-                                navController.popBackStack()
+                            onSpeak = { textToSpeak, isCardFlipped ->
+                                val shouldSpeakSpanish =
+                                    (isCardFlipped && settingsState.answerLocale == SettingLocale.ES.name) ||
+                                            (!isCardFlipped && settingsState.questionLocale == SettingLocale.ES.name)
+                                if (shouldSpeakSpanish) {
+                                    textToSpeechInSpanish.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null, "")
+                                } else {
+                                    textToSpeech.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null, "")
+                                }
                             },
+                            onNavigateBack = navController::popBackStack,
                         )
                     }
+                    composable(
+                        route = "${Routes.SETTINGS.name}/{${NavArgs.LOCALE}}",
+                        arguments = listOf(
+                            navArgument(name = NavArgs.LOCALE.name) {
+                                type = NavType.StringType
+                            },
+                        ),
+                    ) {
+                        SettingsScreen(onNavigateBack = navController::popBackStack)
+                    }
                 }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        if (::textToSpeech.isInitialized) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        if (::textToSpeechInSpanish.isInitialized) {
+            textToSpeechInSpanish.stop()
+            textToSpeechInSpanish.shutdown()
+        }
+        super.onDestroy()
+    }
+
+    private fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.US)
+            textToSpeech.setSpeechRate(1f)
+            textToSpeech.setPitch(0.8f)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "Language is not installed", Toast.LENGTH_SHORT).show()
+                val installIntent = Intent()
+                installIntent.action = TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA
+                startActivity(installIntent)
+            }
+        }
+    }
+
+    private fun onInitSpanish(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeechInSpanish.setLanguage(Locale(SettingLocale.ES.name.lowercase(), SettingLocale.ES.name))
+            textToSpeechInSpanish.setSpeechRate(1f)
+            textToSpeechInSpanish.setPitch(0.8f)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "Language is not installed", Toast.LENGTH_SHORT).show()
+                val installIntent = Intent()
+                installIntent.action = TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA
+                startActivity(installIntent)
             }
         }
     }
