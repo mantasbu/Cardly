6b094d8c0e00d769b9b8797103a1a85a3aed0a1d
Mantas <Mantasb77@gmail.com>
2024-09-29 21:45:32 +0100

    Used AccordionItem to wrap cards

--- app/src/main/java/com/kotlisoft/cardly/presentation/ui/cards/CardsScreen.kt
+++ app/src/main/java/com/kotlisoft/cardly/presentation/ui/cards/CardsScreen.kt
@@ -1,13 +1,10 @@
 package com.kotlisoft.cardly.presentation.ui.cards
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,8 +13,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -35,11 +30,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.kotlisoft.cardly.R
+import com.kotlisoft.cardly.presentation.ui.components.AccordionItem
 import com.kotlisoft.cardly.presentation.ui.components.CardDialog
 import com.kotlisoft.cardly.presentation.ui.components.DeleteDialog
 import com.kotlisoft.cardly.presentation.ui.components.EditDeckNameDialog
@@ -200,32 +195,11 @@ fun CardsScreen(
                         val cardsForLevel = cards.filter { it.level == level }
                         var areLevelCardsVisible by remember { mutableStateOf(true) }
                         if (cardsForLevel.isNotEmpty()) {
-                            Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.Center,
-                                modifier = Modifier
-                                    .padding(8.dp)
-                                    .fillMaxWidth()
-                                    .height(64.dp)
-                                    .background(Color.DarkGray)
-                                    .clickable { areLevelCardsVisible = !areLevelCardsVisible },
+                            AccordionItem(
+                                title = stringResource(id = R.string.level, level),
+                                isExpanded = areLevelCardsVisible,
+                                onExpandChange = { areLevelCardsVisible = !areLevelCardsVisible },
                             ) {
-                                Row {
-                                    Text(
-                                        text = stringResource(id = R.string.level, level),
-                                        style = MaterialTheme.typography.titleMedium,
-                                    )
-                                    Icon(
-                                        imageVector = if (areLevelCardsVisible) {
-                                            Icons.Default.KeyboardArrowDown
-                                        } else {
-                                            Icons.Default.KeyboardArrowUp
-                                        },
-                                        contentDescription = null,
-                                    )
-                                }
-                            }
-                            AnimatedVisibility(visible = areLevelCardsVisible) {
                                 Column(modifier = Modifier) {
                                     cardsForLevel.forEach { card ->
                                         CardItem(
